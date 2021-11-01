685c0effa728aa5a7646d629e56b8c9350753210
cesar <cesar.munuera@gmail.com>
2021-11-01 18:50:07 +0100

    funcionando caja izq

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ function populate(slct1, slct2) {
     }
 }
 
+
 function accionBoton() {
     // Accion del boton
 }
@@ -44,31 +45,21 @@ function inicializarArrayPlatos(platosPricipales) {
             text: element,
             value: element,
         }));
-
     });
-
-
 }
 
 let platosPricipales = ["Ensalada", "Macarrones", "Pure", "Cocido"];
+let preciosPlatosPricipales = [3, 4, 3, 4];
 let platosSegudos = ["Ternera", "Pescado", "Pollo", "Lomo"];
+let preciosPlatosSegudos = [4, 4, 3, 3];
 let platosPostres = ["Manzana", "Helado", "Yogur", "Tarta"];
+let preciosPlatosPostres = [2, 3, 2, 3];
 
 inicializarArrayPlatos(platosPricipales);
 //Hasta aqui
 
 
-
-
-//Ahora la logica de cambiar entre los distintos tipos de platos
-/*$("#radio1").click(function () {
-    if ($("#radio").is(':checked')) {
-        //Llamamos a la funcion de inicializarArrayPlatos
-    } else {
-        //El else supongo que sobra
-    }
-});*/
-
+//Logica de cambiar entre los distintos tipos de platos
 $("#radio1").click(function () {
     inicializarArrayPlatos(platosPricipales);
 });
@@ -80,16 +71,63 @@ $("#radio2").click(function () {
 $("#radio3").click(function () {
     inicializarArrayPlatos(platosPostres);
 });
+//Hasta aqui
 
 
+//Logica de almacenar los platos elegidos
+let mesa1;
+let mesa2;
+let mesa3;
+let mesa4;
+let mesa5;
+var valor;
+
+$("#CajaMenu").on('change', function () {
+    var plato = $(this).val();
+    //Esta variable debe ser uno de los arrays anteriores
+    //Falta la logica de cambiar el array cuando se cambia la mesa
+    $("#CajaPlatosPedidos").prepend($('<option />', {
+        text: plato,
+        value: plato,
+    }));
+});
+//Hasta aqui
 
 
+//Loica de poner los precios
+var parar = false;
+var contador = 0;
+var precio = 0;
 
+platosPricipales.forEach(element => {
+    contador ++; 
+    if(plato == element){
+        parar = true;
+        precio += preciosPlatosPricipales[contador];
+    }
+});
 
+contador = 0;
+if (parar == false){
+    platosSegudos.forEach(element => {
+        contador ++; 
+        if(plato == element){
+            parar = true;
+            precio += preciosPlatosSegudos[contador];
+        }
+    });
+}else if (parar == false){
+    contador = 0;
+    platosPostres.forEach(element => {
+        contador ++; 
+        if(plato == element){
+            parar = true;
+            precio += preciosPlatosPostres[contador];
+        }
+    });
+}
 
+document.getElementById("cajaPrecio").value = "My value";
+//Nose pq no va esta mierda
 
-
-
-
-
-
+//Hasta aqui
