a51ffd6a1f65e2a86c1c3882bd6871ab1c93ea11
César Munuera Pérez <cesar.munuera@gmail.com>
2021-12-06 17:04:07 +0100

    comenzada la p6

--- Practica6/PL6JAVA/target/PL6JAVA-1.0-SNAPSHOT/main.js
+++ Practica6/PL6JAVA/target/PL6JAVA-1.0-SNAPSHOT/main.js
@@ -1,4 +1,4 @@
-$("#boton1").on('click', function () {
+$("#boton").on('click', function () {
 
     var msg = "Los campos erróneos son: ";
 
@@ -11,31 +11,28 @@ $("#boton1").on('click', function () {
     var pais = document.getElementById("pais").value;
     var nombre_coche = document.getElementById("nombre_coche").value;
 
-    if(!(num_vueltas > 40 && num_vueltas < 80)){
+    if (!(num_vueltas > 40 && num_vueltas < 80)) {
         msg += "Número de vueltas " + num_vueltas + " inválido/ ";
     }
 
-    if(!(longitud > 3000 && longitud < 9000)){
+    if (!(longitud > 3000 && longitud < 9000)) {
         msg += "longitud de las vueltas  " + longitud + " invalido/ ";
     }
 
-    if(!(num_curvas > 6 && num_curvas < 20)){
+    if (!(num_curvas > 6 && num_curvas < 20)) {
         msg += "número de curvas " + num_curvas + " invalido/ ";
     }
 
-    if(!(ganancia > 4 && ganancia < 10)){
+    if (!(ganancia > 4 && ganancia < 10)) {
         msg += "ganancia de potencia " + ganancia + " invalido/ ";
     }
 
-    if(nombre_circuito == "" || ciudad == "" || pais == "" || nombre_coche == ""){
+    if (nombre_circuito == "" || ciudad == "" || pais == "" || nombre_coche == "") {
         msg += "Algún campo está vacío.";
     }
 
-    if(msg!="Los campos erróneos son: "){
+    if (msg != "Los campos erróneos son: ") {
         window.alert(msg);
     }
 
-    
-
 });
-
