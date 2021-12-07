cea4f3b5d37e42e22f34394c4f234d1e529327df
César Munuera Pérez <cesar.munuera@gmail.com>
2021-12-07 13:30:00 +0100

    primer servlet a medias

---
+++ Practica6/PL6JAVA/src/main/java/Servlet.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Servlet extends HttpServlet {
+
+    private ModelosDatos bd;
+
+    public void init(ServletConfig cfg) throws ServletException {
+        bd = new ModelosDatos();
+        bd.abrirConexion();
+    }
+
+    // AQUI CODIGO
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        // OBTENEMOS LOS PARAMETROS ENVIADOS DEL POST DEL HTML
+        String nombre_circuito = (String) req.getParameter("nombre_circuito");
+        String ciudad = (String) req.getParameter("ciudad");
+        String pais = (String) req.getParameter("pais");
+        int num_vueltas = Integer.parseInt(req.getParameter("num_vueltas"));
+        int longitud = Integer.parseInt(req.getParameter("longitud"));
+        int num_curvas = Integer.parseInt(req.getParameter("num_curvas"));
+        
+        String nombre_coche = (String) req.getParameter("nombre_coche");
+        float ganancia = Float.parseFloat(req.getParameter("ganancia"));
+        
+        // COMPROBAMOS SI EXISTE EL COCHE
+        if (bd.existeCoche(nombre_coche)) {
+            System.out.println("Ya existe un coche con ese nombre");
+        } else {
+            bd.insertarCoche(nombre_coche,ganancia);
+        }
+        
+        // COMPROBAMOS SI EXISTE EL CIRCUITO
+        if (bd.existeCircuito(nombre_circuito)) {
+            System.out.println("Ya existe un circuito con ese nombre");
+        } else {
+            bd.insertarCircuito(nombre_circuito,ciudad,pais,num_vueltas,longitud,num_curvas);
+        }
+        
+        res.sendRedirect(res.encodeRedirectURL("/PL6JAVA/index.html"));
+    }
+
+    public void destroy() {
+        bd.cerrarConexion();
+        super.destroy();
+    }
+
+}
---
+++ Practica6/PL6JAVA/src/main/webapp/baseDatos.jsp
@@ -0,0 +1,35 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <title>Base de datos</title>
+    <h1>
+        <center>Base de datos</center>
+    </h1>
+
+</head>
+
+
+
+<body>
+    <br>
+    <table>
+        <tr class="cajasConfiguracion">
+            <td>
+                <h2>Configuraci&oacute;n del circuito</h2>
+                <select id="CajaMenu" name="CajaMenu" class="cajaMenus" multiple>
+                </select>
+            </td>
+            <td class="cajaTitulo">
+                <h2>Configuraci&oacute;n del coche</h2>
+                <select id="CajaPlatosPedidos" name="CajaPlatosPedidos" class="cajaMenus" multiple>
+                </select>
+            </td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
